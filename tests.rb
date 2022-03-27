95090925e954fe7c49e4456eb3f122225a9271b3
jlucartc <jlucartc@gmail.com>
2022-03-27 00:05:09 -0300

    Adiciono caso de teste para evento de cancelamento de prorrogamento de prazo 1

--- tests.rb
+++ tests.rb
@@ -227,7 +227,7 @@ RSpec.describe "XML Parser (producao)" do
 	message_desconhecimento_da_operacao = JSON.parse(File.open("./messages/examples/eventos/message_desconhecimento_da_operacao.json","r").read)
 	message_operacao_nao_realizada = JSON.parse(File.open("./messages/examples/eventos/message_operacao_nao_realizada.json","r").read)
 	message_emissao_contingencia = JSON.parse(File.open("./messages/examples/eventos/message_emissao_contingencia.json","r").read)
-	# message_cancelamento_prazo_1 = JSON.parse(File.open("","r").read)
+	message_cancelamento_prazo_1 = JSON.parse(File.open("./messages/examples/eventos/message_cancelamento_prazo_1.json","r").read)
 	# message_cancelamento_prazo_2 = JSON.parse(File.open("","r").read)
 	message_consultar_retorno_autorizacao = JSON.parse(File.open("./messages/examples/message_consultar_retorno_autorizacao.json","r").read)
 	message_consultar_protocolo = JSON.parse(File.open("./messages/examples/message_consultar_protocolo.json","r").read)
@@ -250,7 +250,7 @@ RSpec.describe "XML Parser (producao)" do
 	xml_evento_desconhecimento_da_operacao = generator.desconhecimento_da_operacao(message: message_desconhecimento_da_operacao)
 	xml_evento_operacao_nao_realizada = generator.operacao_nao_realizada(message: message_operacao_nao_realizada)
 	xml_evento_emissao_contingencia = generator.emissao_contingencia(message: message_emissao_contingencia)
-	# xml_evento_cancelamento_prazo_1 = generator.cancelamento_prazo_1(message: message_cancelamento_prazo_1)
+	xml_evento_cancelamento_prazo_1 = generator.cancelamento_prazo_1(message: message_cancelamento_prazo_1)
 	# xml_evento_cancelamento_prazo_2 = generator.cancelamento_prazo_2(message: message_cancelamento_prazo_2)
 	xml_consultar_retorno_autorizacao = generator.consultar_retorno_autorizacao(message: message_consultar_retorno_autorizacao)
 	xml_consultar_protocolo = generator.consultar_protocolo(message: message_consultar_protocolo)
@@ -305,11 +305,11 @@ RSpec.describe "XML Parser (producao)" do
 		expect(schema.valid?(xml_evento_prorrogar_prazo_2)).to be(true)
 	end
 
-	# it "deve criar um xml válido para mensagem de cancelamento de prazo 1" do |test|
-	# 	schema = Nokogiri::XML::Schema(File.open(schema_evento_cancelamento_prazo_1))
-	# 	puts schema.validate(xml_evento_cancelamento_prazo_1) if !schema.valid?(xml_evento_cancelamento_prazo_1)
-	# 	expect(schema.valid?(xml_evento_cancelamento_prazo_1)).to be(true)
-	# end
+	it "deve criar um xml válido para mensagem de cancelamento de prazo 1" do |test|
+		schema = Nokogiri::XML::Schema(File.open(schema_evento_cancelamento_prazo_1))
+		puts schema.validate(xml_evento_cancelamento_prazo_1) if !schema.valid?(xml_evento_cancelamento_prazo_1)
+		expect(schema.valid?(xml_evento_cancelamento_prazo_1)).to be(true)
+	end
 
 	# it "deve criar um xml válido para mensagem de cancelamento de prazo 2" do |test|
 	# 	schema = Nokogiri::XML::Schema(File.open(schema_evento_cancelamento_prazo_2))
